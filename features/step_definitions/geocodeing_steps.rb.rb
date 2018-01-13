8f665c044b6e0028fa634f5b89f7e5ff8efc43ed
tochman <thomas@craftacademy.se>
2018-01-13 13:02:49 +0100

    adds steps for testing map

--- features/step_definitions/geocodeing_steps.rb.rb
+++ features/step_definitions/geocodeing_steps.rb.rb
@@ -1,9 +1,26 @@
 Given(/^I am in Stockholm$/) do
-  Rails.application.config.fake_location = {lat:59.334591,lng:18.063240}
+  Rails.application.config.fake_location = {lat: 59.334591, lng: 18.063240}
+end
+
+Given(/^(?:The user is|I am) at latitude: "([^"]*)", longitude: "([^"]*)"$/) do |lat, lng|
+  Rails.application.config.fake_location = {lat: lat, lng: lng}
 end
 
 Given(/^these restaurants exist:$/) do |table|
   table.hashes.each do |restaurant_hash|
     create(:restaurant, restaurant_hash)
   end
+end
+
+Then(/^(?:I expect a Google map to load|the map has been loaded)$/) do
+  sleep(0.1)
+  expect(page).to have_css '#map .gm-style'
+end
+
+Then(/^the center of the map should be approximately "([^"]*)" lat and "([^"]*)" lng$/) do |lat, lng|
+  ACCEPTED_OFFSET = 0.2
+  center_lat = page.evaluate_script('map.getCenter().lat();')
+  center_lng = page.evaluate_script('map.getCenter().lng();')
+  expect(center_lat).to be_within(ACCEPTED_OFFSET).of(lat.to_f)
+  expect(center_lng).to be_within(ACCEPTED_OFFSET).of(lng.to_f)
 end
--- features/user_can_visit_the_site_and_view_list_of_restaurants.feature
+++ features/user_can_visit_the_site_and_view_list_of_restaurants.feature
@@ -10,9 +10,12 @@ Feature: User can visit the site and view a list of restaurants
       | Scott's Pizza Place | Stockholm |
       | Oliver's Burgers    | Göteborg  |
 
-    And I am in Stockholm
+    #And I am in Stockholm
+  And I am at latitude: "59.33", longitude: "18.05"
 
   Scenario: User can see a list of restaurants
     Given I visit the site
+    And the map has been loaded
+    Then the center of the map should be approximately "59.334591" lat and "18.063240" lng
     Then I should see "Scott's Pizza Place"
     But I should not see "Oliver's Burgers"
